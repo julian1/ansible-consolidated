ed39de655c2367cdc9d19414f11f90dd43f4d136
julian1 <julian.atkinson71@gmail.com>
2016-01-01 21:18:37 +1100

    token location

--- roles/ethereum/files/token.js
+++ roles/ethereum/files/token.js
@@ -10,7 +10,7 @@ web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 ////////////
 // Need to find out how can record the contract...
 // http://pastebin.com/AGDsJL6j
-source = fs.readFileSync('./docker/examples/token.solc' ).toString('ascii');
+source = fs.readFileSync('/root/examples/token.solc' ).toString('ascii');
 compiled = web3.eth.compile.solidity(source);
 contract = web3.eth.contract(compiled.token.info.abiDefinition);
 token = contract.at('0xadd1a267057309c917e1ac794050201378e13b0d');
