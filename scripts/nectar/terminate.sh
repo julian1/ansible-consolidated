7fcbe629947ad0ef4d43cf0bb542f9af14e304a6
julian1 <julian.atkinson71@gmail.com>
2016-10-24 10:08:45 +1100

    fix terminate script

--- scripts/nectar/terminate.sh
+++ scripts/nectar/terminate.sh
@@ -5,6 +5,9 @@ echo "instance details, $INSTANCE"
 
 ID=JA-TEST
 
+
+echo "id is $ID"
+
 nova delete \
   $ID
 
@@ -16,9 +19,15 @@ get_nova_property() {
   grep " $name " $file | cut -d'|' -f 3 | sed 's/ //g'
 }
 
+
+nova show "$ID" > $INSTANCE
 ID=$( get_nova_property id "$INSTANCE"  )
 
-# wait for instance to be terminated
+
+echo "id now $ID"
+
+
+# wait for instance to terminate
 while true; do
   nova show "$ID" > $INSTANCE
   STATE=$( get_nova_property 'OS-EXT-STS:task_state' $INSTANCE )
