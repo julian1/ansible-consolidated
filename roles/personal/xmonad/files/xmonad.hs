aa1464c5164b4a2ffb0798351b34197c1b62581e
julian1 <julian.atkinson71@gmail.com>
2018-04-12 09:04:26 +1000

    laptop, fix font size

--- roles/personal/xmonad/files/xmonad.hs
+++ roles/personal/xmonad/files/xmonad.hs
@@ -14,16 +14,19 @@ import XMonad.Hooks.SetWMName
 
 myterminal :: String
 
--- inherit font size from Xdefaults
 
-myterminal = "xterm -fg white -bg black -fs 12 "
--- myterminal = "xterm "
--- myterminal = "xterm -fa 'DejaVu Sans Mono'  -fg black -bg white "
--- myterminal = "xterm -fa 'DejaVu Sans Mono' -fs 10 -fg black -bg white "
+
+-- myterminal = "xterm -fa 'DejaVu Sans Mono' -fs 10 -fg white -bg black"
+
+-- 2018 GOOD
+-- font and font size will inherit from .xinitrc,
+-- but should be XTerm*faceName: DejaVu Sans Mono, XTerm*faceSize: 11   ! laptop
+myterminal = "xterm  -fg white -bg black"
+
 
 myLogHook dest = dynamicLogWithPP defaultPP { ppOutput = hPutStrLn dest
 ,ppVisible = wrap "(" ")"
-} 
+}
 
 main = do
 xmproc <- spawnPipe "/usr/bin/xmobar /home/meteo/.xmobarrc"
@@ -32,22 +35,20 @@ xmonad $ defaultConfig
         , layoutHook = avoidStruts $ layoutHook defaultConfig
 
     , focusedBorderColor =  "#009900"
-    , normalBorderColor  =  "#666666" 
+    , normalBorderColor  =  "#666666"
 
-		, logHook = myLogHook xmproc 
+		, logHook = myLogHook xmproc
 
-		, terminal = myterminal 
+		, terminal = myterminal
 
 		-- , startupHook = spawnOnce "/usr/bin/xmobar /home/meteo/.xmobarrc"
 
         -- intelij
         , startupHook = setWMName "LG3D"
 
-        } 
+        }
 		`additionalKeys`
-        [ 
+        [
           ((mod1Mask ,  xK_z), spawn "xtrlock -b"),
           ((mod1Mask ,  xK_x), spawn "xtrlock -b")
         ]
-
-
