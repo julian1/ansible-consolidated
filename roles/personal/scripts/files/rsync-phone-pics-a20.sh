9cafaa3bd72448ba790d0df76e91e9ecbdfd47f6
julian1 <git@julian1.io>
2019-07-03 08:31:43 +0659

    update

--- roles/personal/scripts/files/rsync-phone-pics-a20.sh
+++ roles/personal/scripts/files/rsync-phone-pics-a20.sh
@@ -1,19 +1,18 @@
 #!/bin/bash -e
 
 # echo the phone needs a different dest each time, if clear old photes because it resets the DSC count at 0
-# 
+# not sure about a20 phone.
 
-#jmtpfs mnt/phone/
+# jmtpfs mnt/phone/
 
 # dir="/home/meteo/mnt/drive/Pictures/phone/$( date --rfc-3339=date )"
 dst='/mnt/drive/Pictures/phone/a20'
 src='/home/meteo/mnt/phone/Phone/DCIM'
 
-echo $dst
-[ -d $dst ] || mkdir $dst 
+#echo $dst
+#[ -d $dst ] || mkdir $dst 
 
-
+# rsync -n -avP "$src" "$dst"
 rsync -avP "$src"  "$dst"
-
 
 
