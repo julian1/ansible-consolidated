2c35df86f7e0a7d9c6d39903351dfa4f43d5b799
julian1 <git@julian1.io>
2019-08-12 11:25:22 +0659

    comment

--- roles/personal/scripts/files/rsync-phone-pics-a20.sh
+++ roles/personal/scripts/files/rsync-phone-pics-a20.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -e
 
+
+echo 'should just rsync the entire phone sdcard instead as backup'  
+echo 'eg for anki, zalo, etc'
+echo 'eg. just mount the thing then rsync it to large/Phone'
+echo 'we can then work out what photos we want to copy to large/Pictures.' 
+exit
+
+
+####################
+
+
 # echo the phone needs a different dest each time, if clear old photes because it resets the DSC count at 0
 # not sure about a20 phone.
 
