6c9f37fdfb0bd536cb9b64faec6a3985393f81d6
julian1 <mail@julian1.io>
2019-08-26 10:28:20 +0659

    tidy

--- roles/personal/scripts/files/rsync-phone.sh
+++ roles/personal/scripts/files/rsync-phone.sh
@@ -1,12 +1,12 @@
 
 # start simpleSSHD on phone and get the ip, then run
 
-# OK. more reliable than jmtpfs over usb, even if slower. no lockup when read /sdcard.
+# OK. this is reliable for full backup/copy of /sdcard. although slower than jmtfs. no lockup when read /sdcard with sshfs.
 # note it does not work if say '172.16.2.202:/sdcard' without trailing slash
 
 set -x
 
 #ip=172.16.2.202
-rsync -avvzP -e 'ssh -p 2222'  '172.16.2.202:/sdcard/'   /home/large/phone/
+rsync -avvzP -e 'ssh -p 2222' '172.16.2.202:/sdcard/' /home/large/phone/
 
 
