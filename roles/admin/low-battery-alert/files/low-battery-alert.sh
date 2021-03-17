fbe93b04c837d88e1d77b718aa047551952b416e
julian1 <mail@julian1.io>
2021-03-18 06:09:04 +1100

    use systemd timer for low battery alert
    better beep
    
    tidy
    
    update
    
    low battery alert file
    
    tidy
    
    tidy
    
    update
    
    update

--- roles/admin/low-battery-alert/files/low-battery-alert.sh
+++ roles/admin/low-battery-alert/files/low-battery-alert.sh
@@ -2,24 +2,31 @@
 # deployed by ansible!
 
 ## OK - this can be converted to a systemd service by simply having systemd start a loop script
-## eg.  loop.sh 30 low_battery_alert.sh 
+## eg.  loop.sh 30 low_battery_alert.sh
 
-threshold=50
+threshold=90
 capacity=$(cat /sys/class/power_supply/BAT0/capacity)
 status=$(cat /sys/class/power_supply/BAT0/status)
 
 beep() {
+  #https://unix.stackexchange.com/questions/1974/how-do-i-make-my-pc-speaker-beep
+
+  # too short a duration eg. < 0.5s seems to make pop noise, rather than tone
   # $1 duration in seconds, $2 freq
-  timeout -k ${1}s ${1}s speaker-test --frequency ${2} --test sine 2> /dev/null
+  #timeout -k ${1}s ${1}s speaker-test --frequency ${2} --test sine 2> /dev/null
+
+  speaker-test -t sine -f 1000 -l 1 & sleep .5 && kill -9 $!
 }
 
-# test
-# beep 0.1 500
 
-# ok. output redirection will be handled by cron. service / systemd
+# use journalctl to follow log
+# journalctl -f -u low-battery-alert
 echo "$(date) whoami=$(whoami), status=$status, threshold=$threshold, capacity=$capacity"
 
 # beep if below threshold and not charging
 if [ $capacity -le $threshold ] && [ $status != "Charging" ] ; then
-  beep 0.1 500
+  echo "should beep"
+  beep #0.1 500
+else
+  echo "no beep"
 fi
